47f4a4c45023e9bc2642b0922934d9500c7832e8
Branislav Trajkov <branislav.trajkov00@gmail.com>
2024-10-21 12:12:36 +0200

    (db): Users, chats and messages data model and seed data added

---
+++ src/app/seed/seedData.js
@@ -0,0 +1,129 @@
+export const users = [
+  {
+    username: "john.doe",
+    password: "jdoe",
+    email: "john.doe@mail.com",
+    firstName: "John",
+    lastName: "Doe",
+  },
+  {
+    username: "anne.white",
+    password: "awhite",
+    email: "anne.white@mail.com",
+    firstName: "Anne",
+    lastName: "White",
+  },
+  {
+    username: "bill.smith",
+    password: "bsmith",
+    email: "bill.smith@mail.com",
+    firstName: "Bill",
+    lastName: "Smith",
+  },
+  {
+    username: "rachel.williams",
+    password: "rwilliams",
+    email: "rachel.williams@mail.com",
+    firstName: "Rachel",
+    lastName: "Williams",
+  },
+  {
+    username: "mike.johnson",
+    password: "mjohnson",
+    email: "mike.johnson@mail.com",
+    firstName: "Mike",
+    lastName: "Johnson",
+  },
+];
+
+export const chats = [
+  {
+    name: null,
+    description: null,
+    is_group: false,
+  },
+  {
+    name: null,
+    description: null,
+    is_group: false,
+  },
+  {
+    name: null,
+    description: null,
+    is_group: false,
+  },
+  {
+    name: "Friends",
+    description: "Friends groupchat",
+    is_group: true,
+  },
+  {
+    name: "Work",
+    description: "Work groupchat",
+    is_group: true,
+  },
+];
+
+export const messages = [
+  {
+    chat_id: 1,
+    user_id: 1,
+    content: "Hey, how are you?",
+  },
+  {
+    chat_id: 1,
+    user_id: 2,
+    content: "Hi, I'm doing great! How about you?",
+  },
+  {
+    chat_id: 1,
+    user_id: 1,
+    content: "I'm good too, thanks!",
+  },
+  {
+    chat_id: 1,
+    user_id: 2,
+    content: "That's great to hear! Have you been busy lately?",
+  },
+  {
+    chat_id: 1,
+    user_id: 1,
+    content: "Yeah, work has been keeping me occupied. How about you?",
+  },
+  {
+    chat_id: 1,
+    user_id: 2,
+    content: "Same here, it's been hectic but manageable.",
+  },
+  {
+    chat_id: 1,
+    user_id: 1,
+    content: "Any plans for the weekend?",
+  },
+  {
+    chat_id: 1,
+    user_id: 2,
+    content: "Yeah, I'm thinking of going hiking. You?",
+  },
+  {
+    chat_id: 1,
+    user_id: 1,
+    content: "Sounds fun! I might just relax and catch up on some reading.",
+  },
+  {
+    chat_id: 1,
+    user_id: 2,
+    content: "Sounds nice.",
+  },
+];
+
+export const chatsUsers = [
+  {
+    chat_id: 1,
+    user_id: 1,
+  },
+  {
+    chat_id: 1,
+    user_id: 2,
+  },
+];
